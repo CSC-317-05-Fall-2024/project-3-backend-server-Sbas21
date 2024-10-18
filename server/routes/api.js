0f93b6a0455a54ec6ec98b816693e18416320405
Sbas21 <sebastianlim2112@gmail.com>
2024-10-17 23:32:10 -0700

    Revert ""commiting all files""
    This reverts commit a857a70586b81eaf54e9677bdeb64ef044ca1640.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,13 @@
 import express from 'express';
 const router = express.Router();
 
+import { getRestaurants, createRestaurant,deleteRestaurant } from '../data/restaurants.js';
+
 // Add routes here
+
+router.get('/restaurants', (req, res) => {
+    const restaurants = getRestaurants(); // Fetch restaurant data using the function
+    res.render('restaurants', { restaurants }); // Render restaurants.ejs with the data
+});
 
 export {router as backendRouter};
